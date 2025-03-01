4c5a16732176124193ae78a4aea205af95d0f3d3
Megha <meghakanyal395@gmail.com>
2025-03-01 22:37:42 +0530

    daily

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,25 @@
-import { useState } from 'react'
-import './App.css'
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import './App.css';
 
+import Stickywall from './pages/Stickywall';
+import Daily from './pages/Daily';
+import Upcoming from './pages/Upcoming';
+import Sidebar from './component/Sidebar';
 function App() {
-  
   return (
-    <>
-      <h1 className='text-red-400'>hlo</h1> 
-    </>
-  )
+    <Router>
+      <Sidebar/>
+      <Routes>
+        <Route path="/Stickywall" element={<Stickywall />} />
+        <Route path="/daily" element={<Daily />} />
+        <Route path="/upcoming" element={<Upcoming />} />
+      </Routes>
+    </Router>
+  );
 }
 
+<<<<<<< HEAD
 export default App
+=======
+export default App;
+>>>>>>> a4d06a155139611c9292370d2be28719283ace59
