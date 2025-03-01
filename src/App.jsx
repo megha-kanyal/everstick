657cad46cb12435e7c01fea13d3638f69e52693e
Arpit joshi <your-arpitjoshi@gmail.com>
2025-03-01 22:03:43 +0530

    added pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,11 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
   
-
   return (
     <>
-      <h1 className='text-red-400'>hlo</h1>
-       
+      <h1 className='text-red-400'>hlo</h1> 
     </>
   )
 }
