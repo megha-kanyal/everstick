8250edfe8190b4562c4e92e7ff6aa0ae7648bb0a
Megha <meghakanyal395@gmail.com>
2025-03-01 22:34:19 +0530

    daily page

--- src/pages/Daily.jsx
+++ src/pages/Daily.jsx
@@ -2,6 +2,16 @@ import React from 'react'
 
 export default function Daily() {
   return (
-    <div>Daily</div>
+    <div>
+      <div>
+        <h1>Sidebar</h1>
+      </div>
+      <div>
+        <h1>Everstick</h1>
+        <div>
+          <input type="text" />
+        </div>
+      </div>
+    </div>
   )
 }
